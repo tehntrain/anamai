e44da86ebed6ab3f9b602cf3fceb7d50155e06d7
DHDC Main <utehnphnu2015@gmail.com>
2015-03-11 15:27:52 +0700

    แก้ อะรไก็ว่าไป

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,8 +1,23 @@
 <?php
+use yii\helpers\Html;
 /* @var $this yii\web\View */
 $this->title = 'กรมอนามัย';
 ?>
-<div class="jumbotron">
-    Hello Yii2
-    
-</div>
+<?php
+$route1 = Yii::$app->urlManager->createUrl('test/test1');
+?>
+<a href="<?=$route1?>">ไปที่ test1</a>
+<br>
+
+<?php
+$route2 = Yii::$app->urlManager->createUrl(['test/test2','name'=>'utehn','lname'=>'JAD']);
+?>
+<a href="<?=$route2?>">ไปที่ test2</a>
+<br>
+
+<?=
+Html::a('ลิงค์แบบที่ 3', ['test/test1','a'=>'1']);
+?>
+
+
+
