f9c6f861893b3985a5301067fb3919baec56440b
DHDC Main <utehnphnu2015@gmail.com>
2015-03-13 09:39:25 +0700

    แก้ อะรไก็ว่าไป

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -55,6 +55,44 @@ $this->registerJsFile('./js/chart_dial.js');
        <h4>หญิงมีครรภ์ได้รับการฝากครรภ์ครั้งแรก<br>ก่อน 12 สัปดาห์</h4>
         <div id="ch1"></div>
     </div>
+     <div class="col-sm-4" style="text-align: center;">
+        <?php        
+        $target = 503;
+        $result = 102;
+        $persent = 0.00;
+        if ($target > 0) {
+            $persent = $result / $target * 100;
+            $persent= number_format($persent, 2);
+        }
+        $base = 90;
+        $this->registerJs("
+                        var obj_div=$('#ch2');
+                        gen_dial(obj_div,$base,$persent);
+                    ");
+        ?>
+       <h4>หญิงมีครรภ์ได้รับการฝากครรภ์ครั้งแรก<br>ก่อน 12 สัปดาห์</h4>
+        <div id="ch2"></div>
+        
+    </div>
+    
+     <div class="col-sm-4" style="text-align: center;">
+        <?php        
+        $target = 503;
+        $result = 102;
+        $persent = 0.00;
+        if ($target > 0) {
+            $persent = $result / $target * 100;
+            $persent= number_format($persent, 2);
+        }
+        $base = 90;
+        $this->registerJs("
+                        var obj_div=$('#ch3');
+                        gen_dial(obj_div,$base,$persent);
+                    ");
+        ?>
+       <h4>หญิงมีครรภ์ได้รับการฝากครรภ์ครั้งแรก<br>ก่อน 12 สัปดาห์</h4>
+        <div id="ch3"></div>
+    </div>
 </div>
 
 
